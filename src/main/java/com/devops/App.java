b400f834b99c651f14b235adb60e46a9adf2296b
EC2 Default User <ec2-user@ip-172-31-46-26.ap-northeast-1.compute.internal>
2023-01-10 06:30:09 +0000

    to this world git status

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!"I am back );
+        System.out.println( "Hello World!"I am back to this world!!);
     }
 }
