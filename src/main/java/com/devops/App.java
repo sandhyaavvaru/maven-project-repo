35c7f133b1c3b45a1223f1905687f007583cbbf5
EC2 Default User <ec2-user@ip-172-31-46-26.ap-northeast-1.compute.internal>
2023-01-10 06:40:57 +0000

    hello hello

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!"I am back to this world!!);
+        System.out.println( "Hello World!"I am back to this world hello helo!!);
     }
 }
